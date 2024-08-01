1126d720437d9dc517a112290ea9454c071b16af
TomJensWilliams <tomjenswilliams@gmail.com>
2024-08-01 02:17:19 -0700

    Create server.js

---
+++ GettingStarted/server.js
@@ -0,0 +1,30 @@
+// Remember: npm i fastify
+// Remember: {"type": "module"}
+
+// Require the framework and instantiate it
+
+// ESM
+import Fastify from 'fastify';
+const fastify = Fastify({
+  logger: true
+});
+
+/*
+// CommonJS
+const fastify = require('fastify')({
+  logger: true
+});
+*/
+
+fastify.get('/', function(request, reply) {
+  reply.send({ hello: 'world' });
+});
+
+// Run the server!
+fastify.listen({ port: 3000}, function(err, address) {
+  if(err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  // Server is now listening on ${address}
+});
